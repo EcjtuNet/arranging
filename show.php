f8c2cd6e32962f73a8c6d4182bb2d7d456831d06
zvenshy <zvenshy@gmail.com>
2014-09-14 17:29:28 +0800

    test

--- show.php
+++ show.php
@@ -8,7 +8,30 @@
 
     <!-- Bootstrap -->
     <link href="/css/bootstrap.min.css" rel="stylesheet">
-
+    <link rel="stylesheet" href="/js/jquery-1.11.0.min.js">
+    <style type="text/css">
+    	p.lead em{
+    		font-size: 72px;
+    	}
+		.nav-div{
+			display: none;
+		}
+		.show{
+			display: block;
+		}
+    </style>
+	<script>
+		$(document).ready(function(){
+			$('.nav li').click(function(ev){
+				ev.preventDefault();
+				var ind = $(this).index();
+				$(this).siblings('.active').removeClass('active')
+				.end().addClass('active');
+				$('.nav-div').eq(ind).addClass('show')
+					.siblings('.show').removeClass('show');
+			});
+		});
+	</script>
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
     <!--[if lt IE 9]>
@@ -37,47 +60,54 @@
 					<a href="http://www.ecjtu.net/about/m/" target="_blank">关于日新</a>
 				</li>
 			</ul>
-			<div class="alert alert-success">
-				请牢记您的号码，预计时间仅供参考
+			<div class="nav-div show">
+				<div class="alert alert-success">
+					请牢记您的号码，预计时间仅供参考
+				</div>
+				<p>
+					您的号码是：
+				</p>
+				<p class="lead text-success text-center">
+					<em><?php echo $id; ?></em>
+				</p>
+				<p>
+					<em>*预计在<b><?php echo $time; ?></b>面试，请做好准备</em>
+				</p>
 			</div>
-			<p>
-				您的号码是：
-			</p>
-			<p class="lead text-success text-center">
-				<em><?php echo $id; ?></em>
-			</p>
-			<p>
-				<em>*预计在<b><?php echo $time; ?></b>面试，请做好准备</em>
-			</p>
-			<dl class="dl-horizontal">
-				<dt>
-					网站建设部
-				</dt>
-				<dd>
-					负责日新网主页和各WEB产品等的开发与维护，旗下分前后端手机端程序员，
-				</dd>
-				<dd>
-					需要掌握HTML/CSS、Javascript、Web后端开发语言（如PHP）。
-				</dd>
-				<dt>
-					体验设计部
-				</dt>
-				<dd>
-					负责日新网主页和各产品等的设计与出图，旗下分体验设计师和交互设计师，
-				</dd>
-				<dd>
-					需要掌握PS以及拥有良好的审美观。
-				</dd>
-				<dt>
-					安全运维部
-				</dt>
-				<dd>
-					负责开发维护日新网服务器，保证全站的正常运行以及性能优化，
-				</dd>
-				<dd>
-					需要熟练掌握Linux以及数据库等软件的安装调试维护优化。
-				</dd>
-			</dl>
+			<div class="nav-div">
+				
+			</div>
+			<div class="nav-div">
+				<dl class="dl-horizontal">
+					<dt>
+						网站建设部
+					</dt>
+					<dd>
+						负责日新网主页和各WEB产品等的开发与维护，旗下分前后端手机端程序员，
+					</dd>
+					<dd>
+						需要掌握HTML/CSS、Javascript、Web后端开发语言（如PHP）。
+					</dd>
+					<dt>
+						体验设计部
+					</dt>
+					<dd>
+						负责日新网主页和各产品等的设计与出图，旗下分体验设计师和交互设计师，
+					</dd>
+					<dd>
+						需要掌握PS以及拥有良好的审美观。
+					</dd>
+					<dt>
+						安全运维部
+					</dt>
+					<dd>
+						负责开发维护日新网服务器，保证全站的正常运行以及性能优化，
+					</dd>
+					<dd>
+						需要熟练掌握Linux以及数据库等软件的安装调试维护优化。
+					</dd>
+				</dl>
+			</div>
 		</div>
 	</div>
 </div>
